f0fda256a859605ceec247fea682b1c0c0113658
mortem <lampakyu@gmail.com>
2022-06-15 00:22:18 +0100

    correct fixes

--- src/test/FactoryTest.t.sol
+++ src/test/FactoryTest.t.sol
@@ -1,7 +1,4 @@
 // SPDX-License-Identifier: UNLICENSED
-
-// The BundleTest is old. 
-// This is the test that I am using to build
 pragma solidity 0.8.11;
 
 // import "ds-test/test.sol";
@@ -19,12 +16,6 @@ import {License} from "../License.sol";
 import {MockERC20} from "solmate/test/utils/mocks/MockERC20.sol";
 import {Factory} from "../Factory.sol";
 import {Authority} from "solmate/auth/Auth.sol";
-
-// interface CheatCodes {
-//     function deal(address who, uint256 newBalance) external;
-//     function addr(uint256 privateKey) external returns (address);
-//     function warp(uint256) external;    // Set block.timestamp
-// }
 
 
 contract FactoryTest is DSTestPlus {
@@ -44,48 +35,4 @@ contract FactoryTest is DSTestPlus {
 
     function testLicenseAuthorities() public {
     }
-
-
-    // My hypothesis was that, right now, the owner and callables of the four contracts are as follows (RN) right now: 
-
-    // +------------------+--------------------------------------------------------------------------+----------------------------------------------------------------------------------------+
-    // |     Contract     |                                  Owner                                   |                                      Callable by?                                      |
-    // +------------------+--------------------------------------------------------------------------+----------------------------------------------------------------------------------------+
-    // | Factory          | ALICE (artist)                                                           | Anyone                                                                                 |
-    // | License          | Auth(msg.sender).owner()= owner of the factory contract, therefore Alice | those with validLicenses of that license (this has not been verified in the tests yet) |
-    // | AuthorityModule | not owned                                                                | Anyone                                                                                 |
-    // | AccessToken      | Auth(msg.sender).owner()= owner of the factory contract, therefore Alice | those with validLicenses of that license                                               |
-    // +------------------+--------------------------------------------------------------------------+----------------------------------------------------------------------------------------+
-
-    // In practice, we would like the structure to be (I) - ideal:
-
-    //   +------------------+--------------------------------------+----------------------+
-    //   |     Contract     |                Owner                 |     Callable by?     |
-    //   +------------------+--------------------------------------+----------------------+
-    //   | Factory          | Us                                   | Anyone               |
-    //   | License          | Owner of MasterCopy (Artist - Alice) | Owner of MasterCopy  |
-    //   | AuthorityModule | Us / Not owned                       | Anyone               |
-    //   | AccessToken      | Owner of MasterCopy (Artist - Alice) | validLicense holders |
-    //   | MasterCopy       | Artist                               | Artist               |
-    //   +------------------+--------------------------------------+----------------------+
-
-// My initial plan was to verify that these contracts are indeed behaving like (RN), and then I shall modify them to become like (I)
-
-// Two observations here: 
-
-// (1) the AuthorityModule seems to be controlling the wrong contract
-// The license contract requiresAuth modifier calls the canCall function of the AuthorityModule to check if someone could call functions of the license contract
-// the AuthorityModule then calls the license contract to see if the user in question has a validLicense 
-// This appears wrong to me. validLicense holders should have rights to call the functions of the AccessToken (to mint AccessTokens), not to call the functions of the license itself
-// That right is reserved for the MasterCopy holder. 
-
-// (2) To test my hypothesis as to whether the contracts are really behaving like (RN), we need to check who's the contracts callable by. 
-// As per the contract set up, it is not possible to call the contracts 
-// For Alice to call the License contract, she needs to have a validLicense
-// But for her to have a valid license, she needs to be minted one
-// But the mint function is guarded by the requiresAuth modifier - so she can't call the function
-
-// I suppose this can be done by calling the setAuthority() function on the license contract, but I haven't tried that out yet. 
-
-// Final remark, aside from (1), (correct me if my interpretation is wrong) - the complicated authorities can be set straight not by rewriting the contracts but by calling the apppropriate setAuthority() functions. 
 }
